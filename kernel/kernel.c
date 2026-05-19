00e37771c9220f37679f33e2421b9b045d7047ca
DragonShip20 <wardzinski.antoni@gmail.com>
2026-05-19 13:57:03 +0200

    vesa: add font
    Add 8x8 VGA style font (for now just letters A-Z)

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -15,16 +15,14 @@ void kernel_main() {
     int screen_w = width / 10;
     int screen_h = height / 10;
 
-    /* Floods the screen with A */
-    for (int x=0; x<screen_w; x++) {
-        for (int y=0; y<screen_h; y++) {
-            vscreen[y * screen_w + x].fg = fg;
-            vscreen[y * screen_w + x].bg = bg;
-            for (int i=0; i<8; i++) {
-                vscreen[y * screen_w + x].glyph[i] = font['A'][i];
-            }
-            draw_cell(vscreen[y * screen_w + x], x, y);
+    /* Dumps the font to the screen */
+    for (int x=65; x<91; x++) {
+        vscreen[x].fg = fg;
+        vscreen[x].bg = bg;
+        for (int i=0; i<8; i++) {
+            vscreen[x].glyph[i] = font[x][i];
         }
+        draw_cell(vscreen[x], x-65, 0);
     }
     flush();
         
