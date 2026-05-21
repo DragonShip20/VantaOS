153623d756215d6ef042933d9dd76460fa5df184
DragonShip20 <wardzinski.antoni@gmail.com>
2026-05-21 19:07:17 +0200

    vesa: completed font

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -11,26 +11,13 @@ void kernel_main() {
     init_idt();
     init_vesa();
 
-    putc('H');
-    vesa_set_cursor(1, 0);
-    putc('E');
-    vesa_set_cursor(2, 0);
-    putc('L');
-    vesa_set_cursor(3, 0);
-    putc('L');
-    vesa_set_cursor(4, 0);
-    putc('O');
-    vesa_set_cursor(0, 1);
-    putc('W');
-    vesa_set_cursor(1, 1);
-    putc('O');
-    vesa_set_cursor(2, 1);
-    putc('R');
-    vesa_set_cursor(3, 1);
-    putc('L');
-    vesa_set_cursor(4, 1);
-    putc('D');
-    vesa_set_cursor(5, 1);
+    /* Dump the font to the screen */
+    for (int i=32; i<127; i++) {
+        putc(i);
+        vesa_set_cursor(vcursor.x + 1, vcursor.y);
+        if (i==47 || i==64 || i==90 || i==96 || i==122)
+            vesa_set_cursor(0, vcursor.y + 1);
+    }
     render_cells();
     
     while (1) {
